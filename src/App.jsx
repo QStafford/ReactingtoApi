bf5e8182748e50cb2386a4beeb351ea983d64071
Quincy S <xXQS316Xx@gmail.com>
2021-10-21 13:53:00 -0500

    finished lab now for styling

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,11 @@ const App = () => {
         const res = await fetch('https://ghibliapi.herokuapp.com/films')
         const filmsData = await res.json();
         setFilms(filmsData)
+
+        //=== no async 
+        // fetch('https://ghibliapi.herokuapp.com/films')
+        //     .then(res => res.json())
+        //     .then(filmsData => setFilms(filmsData))
     }
     const getPpl = async () => {
         const res = await fetch('https://ghibliapi.herokuapp.com/people')
@@ -16,9 +21,9 @@ const App = () => {
     }
     return (
         <div>
-            <h1>Hello from App Component!</h1>
+            <h1>Films and Stars</h1>
             <button onClick={getFilms}>Load Films!</button>
-            {films.map(films => <div className="col-md-6" key={films.title}>
+            {films.map(films => <div className="col-md-6" key={films.id}>
                 <div className="card shadow my-2">
                     <div className="card-body">
                         <h4 className="card-title">{films.title}</h4>
@@ -30,11 +35,12 @@ const App = () => {
             )}
             <button onClick={getPpl}>Load People!</button>
             {people.map(people => <div className="col-md-6" key={people.name}>
-                <div className="card shadow my-2">
+                <div className="card">
                     <div className="card-body">
                         <h4 className="card-title">{people.name}</h4>
                         <p className="cardsubtitle text-muted">{people.gender}</p>
-                        <p className="card-text">{people.age}</p>
+                        <p className="card-text">{people.age} years old, {people.eye_color} eys, {people.hair_color} hair </p>
+                        <a href={people.films}  target="_blank">JSON</a>
                     </div>
                 </div>
             </div>
