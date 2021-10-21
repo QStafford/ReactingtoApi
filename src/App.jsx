02582c9a0394ae88abc594ac4510ac4249e66eee
Quincy S <xXQS316Xx@gmail.com>
2021-10-21 11:20:36 -0500

    got it to finally print

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { useEffect, useState } from "react";
 const App = () => {
+    const [films, setFilms] = useState([]);
+    
+    const getFilms = async () => {
+        const res = await fetch('https://ghibliapi.herokuapp.com/films')
+        const filmsData =await res.json();
+        setFilms(filmsData)
+    }
     return (
         <div>
             <h1>Hello from App Component!</h1>
+            <button onClick = {getFilms}>Load Films!</button>
+            {films.map (films => <div key = {films.title}>{films.title}</div> )}
         </div>
+
     );
 };
 
